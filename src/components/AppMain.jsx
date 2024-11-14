e570954e10bf97983f69f7fb63cd7e8a185a9c6e
deca-francesco <francescodecarlo70@gmail.com>
2024-11-14 12:10:43 +0100

    ADD: colors on tags and array with no duplicates of tags

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -4,7 +4,13 @@ import posts from "../data/posts.js"
 export default function AppMain() {
 
 
-
+    // con map facciamo l'array di tutti gli array di tags.
+    // con flat portiamo l'array di array ad un array di un livello solo.
+    // con il costruttore Set avremo un oggetto contenente i tag senza duplicati.
+    // tutto è nelle quadre, quindi avremo un array contenente quell'oggetto con tutti i tag.
+    // con l'operatore spread esplodiamo l'oggetto.
+    const allTags = [... new Set(posts.map(posts => posts.tags).flat())]
+    console.log(allTags);
 
 
     return (
@@ -14,7 +20,10 @@ export default function AppMain() {
                     {/* ciclo con map e solo se published è true restituisco il post */}
                     {posts.map(post => post.published && <PostCard key={post.id} data={post} />)}
                 </section>
-
+                <section className="tags_section">
+                    <h3>Tag dei post senza duplicati</h3>
+                    {allTags.join(" | ")}
+                </section>
             </div>
         </main>
     )
--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -4,37 +4,7 @@ import Button from "../Button/Button.jsx"
 export default function PostCard({ data }) {
 
 
-    function tagColor(array) {
 
-        array.forEach(tag => {
-            if (tag === "html") {
-                return tag.classList.add("green")
-            } else if (tag === "css") {
-                return <span style="color: blue">{tag}</span >
-            } else if (tag === "js") {
-                return <span style="color: red">{tag}</span >
-            } else if (tag === "php") {
-                return <span style="color: yellow">{tag}</span >
-            }
-        })
-    }
-
-
-
-
-
-    const postTag = data.tags.forEach(tag => {
-        return <span className="post_tag">{tag}</span>
-    })
-
-    function tagClass(className) {
-        const post_tagEl = document.querySelectorAll(".post_tag")
-
-        console.log(post_tagEl);
-
-    }
-
-    tagClass(postTag)
 
     return (
         <div className={style.post_card}>
@@ -45,19 +15,8 @@ export default function PostCard({ data }) {
                 <h3>{data.title}</h3>
                 <p>{data.content}</p>
                 <p className={style.tags}>
-                    {data.tags.map(tag => {
-
-                        if (tag == "html") {
-                            return <span style="color: green">{tag}</span >
-                        } else if (tag == "css") {
-                            return <span style="color: blue">{tag}</span >
-                        } else if (tag == "js") {
-                            return <span style="color: red">{tag}</span >
-                        } else if (tag == "php") {
-                            return <span style="color: yellow">{tag}</span >
-                        }
-
-                    })}
+                    {/* per ogni tag ritorno uno span con con la classe = al tag e contenente il tag */}
+                    {data.tags.map((tag, index) => <span key={index} className={style[tag]}>{tag} </span>)}
                 </p>
                 <Button />
             </div>
